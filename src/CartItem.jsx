82961e9c52e3b4b622b65ca73be7ed17c36f0385
boldb <boldbayarjantsan@gmail.com>
2025-03-13 01:14:10 -0400

    finished

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -11,13 +11,15 @@ const CartItem = ({ onContinueShopping }) => {
   const calculateTotalAmount = () => {
     let total = 0;
     cart.forEach(item => {
-        let cost = parseFloat(item.cost.substring(1));
-        total += item.quantity * cost;
-    })
+        const unitPrice = parseFloat(item.cost.substring(1)); 
+        total += unitPrice * item.quantity; 
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-      alert('Functionality to be added for future reference');
+    e.preventDefault(); 
+    onContinueShopping(e);
   };
 
   const handleIncrement = (item) => {
@@ -25,15 +27,24 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleDecrement = (item) => {
-   dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    if (item.quantity > 1) {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+      } else {
+        dispatch(removeItem(item.name)); // Remove item when quantity reaches 0
+      } 
   };
 
   const handleRemove = (item) => {
-    
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const unitPrice = parseFloat(item.cost.substring(1)); // Extract numeric value from cost string
+    return item.quantity * unitPrice;
+  };
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   return (
